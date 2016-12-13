d3f5d60642a6b47425c6cb8c1090bfcc0a7dd1b6
William Poehlman <wpoehlm@clemson.edu>
2016-12-13 12:20:12 -0500

    update read mapping

--- 03-Map-reads.sh
+++ 03-Map-reads.sh
@@ -5,6 +5,8 @@ mkdir $BASEDIR/Alignment
 cp $BASEDIR/Templates/basedir.txt $BASEDIR/Alignment
 
 tar -xf $BASEDIR/Software/samtools-1.3.1.tar.gz -C $BASEDIR/Software
+unzip $BASEDIR/Software/hisat2-2.0.5-Linux_x86_64.zip -d \
+$BASEDIR/Software
 
 for i in `cat $BASEDIR/SRAList.txt` ; do cat $BASEDIR/Templates/HISAT.template \
 | sed s/SRA/$i/g > $BASEDIR/Alignment/$i.hisat.pbs ; done
