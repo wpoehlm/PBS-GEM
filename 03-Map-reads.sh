b05678016bbffb604cb549796281cdd17d277d6f
William Poehlman <wpoehlm@clemson.edu>
2016-12-13 12:11:44 -0500

    update read mapping

--- 03-Map-reads.sh
+++ 03-Map-reads.sh
@@ -8,3 +8,11 @@ tar -xf $BASEDIR/Software/samtools-1.3.1.tar.gz -C $BASEDIR/Software
 
 for i in `cat $BASEDIR/SRAList.txt` ; do cat $BASEDIR/Templates/HISAT.template \
 | sed s/SRA/$i/g > $BASEDIR/Alignment/$i.hisat.pbs ; done
+
+cd $BASEDIR/Alignment
+
+for i in *hisat.pbs ; do qsub -o $BASEDIR/Logs/$i.out \
+-e $BASEDIR/Logs/$i.err ./$i ; done
+
+mkdir PBS
+mv *pbs PBS
