e6c73b9ba8da32a04e4f9160516854a31d71a2be
William Poehlman <wpoehlm@clemson.edu>
2016-12-14 11:00:27 -0500

    update stringtie

--- 04-Count-transcripts.sh
+++ 04-Count-transcripts.sh
@@ -10,6 +10,13 @@ $BASEDIR/Software
 for i in `cat SRAList.txt` ; do cat $BASEDIR/Templates/STRINGTIE.template \
 | sed s/SRA/$i/g > $BASEDIR/Count/$i.stringtie.pbs ; done
 
+cd $BASEDIR/Count
+
+for i in *tie.pbs ; do qsub -o $BASEDIR/Logs/$i.out \
+-e $BASEDIR/Logs/$i.err ./$i ; done
+
+mkdir PBS
+mv *pbs PBS
 
 
 
