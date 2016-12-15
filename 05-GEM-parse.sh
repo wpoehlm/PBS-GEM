a3cd2eadc34546f4206f175a9d28143c417c1764
William Poehlman <wpoehlm@clemson.edu>
2016-12-15 16:58:36 -0500

    update GEM parse

--- 05-GEM-parse.sh
+++ 05-GEM-parse.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 BASEDIR=$(pwd)
-mkdir GEM
+mkdir $BASEDIR/GEM
 mkdir $BASEDIR/GEM/transcripts
 cp $BASEDIR/Templates/basedir.txt $BASEDIR/GEM
+cp $BASEDIR/Templates/basedir.txt $BASEDIR/GEM/transcripts
 
+cd $BASEDIR/Templates
 
+qsub -o $BASEDIR/Logs/GEM.out -e $BASEDIR/Logs/GEM.err \
+./GEM.template
+
+cd $BASEDIR
