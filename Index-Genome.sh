0d503991791d3d07022c1dbe3e62706aa0a59497
William Poehlman <wpoehlm@clemson.edu>
2017-06-11 13:02:27 -0400

    Cleaned up Reference index scripts

--- Index-Genome.sh
+++ Index-Genome.sh
@@ -2,16 +2,10 @@
 
 REF_PREFIX=$1
 
-pwd > basedir.txt
-BASEDIR=$(pwd)
-cp basedir.txt $BASEDIR/Templates 
-cp basedir.txt $BASEDIR/Logs/Progress
-rm $BASEDIR/basedir.txt
-
 
 cat ./Templates/Index.template | sed s/REF_PREFIX/$REF_PREFIX/g >> \
-$BASEDIR/Reference/RefIndex.pbs
+./Reference/RefIndex.pbs
 
-cd $BASEDIR/Reference/
+cd ./Reference/
 
 qsub -o $BASEDIR/Logs/Index.out -e $BASEDIR/Logs/Index.err ./RefIndex.pbs
